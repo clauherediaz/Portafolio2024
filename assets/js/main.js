223f4cecbfa2810e95b83ec25af6c98c2686acd3
clauhere <clauherediaz@gmail.com>
2024-08-23 19:43:25 -0400

    Proyecto Portafolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,15 @@
-
-$(function () {
+$(function() {
     $('[data-toggle="tooltip"]').tooltip();
-    $('#enviarCorreo').click(function (){
-        alert("El correo fue enviado correctamente...")
+    $('#enviarCorreo').click(function() {
+        alert("El correo fue enviado correctamente")
     });
-    $('body').on('dblclick','h2',function () {
-        $(this).css("color",'green');
+    $('body').on('dblclick', 'h2', function() {
+        $(this).css("color", 'green');
     });
-    $('.card-title').click(function(){
+    $('.card-title').click(function() {
         $('.card-text').toggle();
     });
     $('.carousel').carousel({
         interval: false
     })
 });
-
